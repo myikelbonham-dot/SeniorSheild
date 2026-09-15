17454b0ec405f10909516e94b0ccc3491f4bcd35
myikelbonham-dot <myikelbonham@gmail.com>
2026-09-14 20:41:29 -0400

    Update scam_patterns.py

--- scam_patterns.py
+++ scam_patterns.py
@@ -1,1 +1,34 @@
+URGENT_WORDS = [
+    "urgent",
+    "immediately",
+    "act now",
+    "right away",
+    "expires today",
+    "final warning"
+]
 
+MONEY_WORDS = [
+    "send money",
+    "payment",
+    "gift card",
+    "wire transfer",
+    "cash app",
+    "venmo",
+    "bitcoin"
+]
+
+PERSONAL_INFO_WORDS = [
+    "password",
+    "social security",
+    "bank account",
+    "credit card",
+    "security code",
+    "verification code"
+]
+
+SUSPICIOUS_LINK_WORDS = [
+    "click here",
+    "click the link",
+    "verify your account",
+    "login here"
+]
